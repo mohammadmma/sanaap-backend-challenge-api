27ba6259183327ee62033b093bfafec9033d34bb
mohammadmma <mohammadmma3004@gmail.com>
2026-05-10 21:53:41 +0330

    feat: implement admin request model and actions for approval and rejection in admin panel

--- backend/apps/authentication/urls.py
+++ backend/apps/authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from apps.authentication.views import RegisterView, LoginView, LogoutView, MeView
+from apps.authentication.views import ViewerRegisterView, LoginView, LogoutView, MeView, AdminDemadView
 
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
-    path('register/', RegisterView.as_view(), name="register"),
+    path('register/', ViewerRegisterView.as_view(), name="register"),
+    path('admin-demand/', AdminDemadView.as_view(), name="admin-demand"),
     path('me/', MeView.as_view(), name="me"),
 ]
--- backend/apps/authentication/views.py
+++ backend/apps/authentication/views.py
@@ -6,12 +6,38 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import status
 from django.contrib.auth import get_user_model
 from apps.authentication.serializers import RegisterValidator
+from apps.authentication.models import AdminRequestModel
+from django.db import transaction
+
 
 User = get_user_model()
 
 
 
-class RegisterView(APIView):
+class AdminDemadView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegisterValidator(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            with transaction.atomic():
+                user = User.objects.create_user(**serializer.validated_data)
+
+                viewer_group, _ = Group.objects.get_or_create(name='viewer')
+                user.groups.add(viewer_group)
+
+                login(request, user)
+
+                AdminRequestModel.objects.create(user=user)
+                return Response(
+                    {
+                        'message': 'Registered as viewer. Admin request is pending superuser approval.',
+                        'user': {'id': user.id, 'username': user.username, 'role': user.groups.values_list('name', flat=True).first()},
+                    },
+                    status=status.HTTP_201_CREATED
+                )
+
+class ViewerRegisterView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
